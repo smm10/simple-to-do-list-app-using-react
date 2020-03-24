bfaffe9dfa323ca3aac9a46582750a18522d5ef6
smm10 <smmalesh2000@gmail.com>
2020-03-25 01:22:20 +0530

    somewhat done with the layout have to think about the onClick functions and all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      to_do_list:[]
+    }
+  }
+
+
+
+
+  render(){
+    return(
+      <div className="main">
+      <h3>Enter your to-do-items here:</h3><input type="text"></input>
+      <button onClick>Submit</button>
     </div>
-  );
-}
+    );
+
+  }
+  }
+  
 
 export default App;
