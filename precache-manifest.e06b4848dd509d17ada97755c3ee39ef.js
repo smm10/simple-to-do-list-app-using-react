f4dbdc331c1a91d8485cf54edebd6fde77eb181d
smm10 <smmalesh2000@gmail.com>
2020-03-30 18:35:42 +0530

    Updates

---
+++ precache-manifest.e06b4848dd509d17ada97755c3ee39ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54c682c4fffc921cedca3b75870e5c5",
+    "url": "/simple-to-do-list-app-using-react/index.html"
+  },
+  {
+    "revision": "115d073aa94f3d726ad6",
+    "url": "/simple-to-do-list-app-using-react/static/css/main.1571b7df.chunk.css"
+  },
+  {
+    "revision": "481754f2a8a99e1f5f71",
+    "url": "/simple-to-do-list-app-using-react/static/js/2.ca21cde3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-to-do-list-app-using-react/static/js/2.ca21cde3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115d073aa94f3d726ad6",
+    "url": "/simple-to-do-list-app-using-react/static/js/main.731ca812.chunk.js"
+  },
+  {
+    "revision": "a9679b283247a4690a4e",
+    "url": "/simple-to-do-list-app-using-react/static/js/runtime-main.8007f7ad.js"
+  }
+]);
